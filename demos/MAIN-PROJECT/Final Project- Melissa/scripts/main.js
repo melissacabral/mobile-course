4b6d971ca94c4389d5da455c9b50d65f74229889
melissacabral <melissacabral@gmail.com>
2013-06-21 16:54:51 -0700

    main proj - lots of CSS cleanup, reducing complexity
    getting rid of cruft from Lynda version. much simpler CSS and JS now.

--- demos/MAIN-PROJECT/Final Project- Melissa/scripts/main.js
+++ demos/MAIN-PROJECT/Final Project- Melissa/scripts/main.js
@@ -5,10 +5,12 @@ $(document).ready(function() {
   $('body').addClass('js');
   var $menu = $('#menu'),
     $menulink = $('.menu-link');
+    $iconlink = $('.menu-link i');
   
 $menulink.click(function() {
   $menulink.toggleClass('active');
   $menu.toggleClass('active');
+  $iconlink.toggleClass('icon-rotate-90'); //nifty font awesome icon rotation
   return false;
 });});
 
