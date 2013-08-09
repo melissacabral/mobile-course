f959df1d310bc9e19497e001482f5b2eb6303a29
tiziana <tiziana@platt.edu>
2013-08-08 19:52:06 -0700

    form demos updates

---
+++ demos/navDemos/flyout_basic/flyout.js
@@ -0,0 +1,15 @@
+<script>
+$(document).ready(function() {
+  $('body').addClass('js');
+  
+  var $menu = $('#menu'),
+    $menulink = $('.menu-link'),
+    $wrap = $('#wrap');
+  
+  $menulink.click(function() {
+    $menulink.toggleClass('active');
+    $wrap.toggleClass('active');
+    return false;
+	});
+});
+</script>
